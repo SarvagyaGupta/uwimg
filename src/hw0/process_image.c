d0027699e85f6706fe754cfc80ce5b71b84239ac
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-10-08 15:15:13 -0700

    Getting and setting pixels, copying images, and grayscale images done

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -4,21 +4,26 @@
 #include <math.h>
 #include "image.h"
 
+#define get_index(im, x, y, c) ((x) + ((im.w) * (y)) + ((im.w) * (im.h) * (c)))
+#define get_inbound_index(a, min, max) (((a) > max) ? max : (((a) < min) ? min : (a)))
+#define in_bounds(im, x, y, c) ((x) < im.w && (y) < im.h && (c) < im.c)
+
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    return im.data[get_index(im, get_inbound_index(x, 0, im.w - 1), get_inbound_index(y, 0, im.h - 1)
+            , get_inbound_index(c, 0, im.c - 1))];
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+    if (!in_bounds(im, x, y, c)) return;
+    im.data[get_index(im, x, y, c)] = v;
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
+    memcpy(copy.data, im.data, im.w * im.h * im.c * sizeof(float));
     return copy;
 }
 
@@ -26,7 +31,15 @@ image rgb_to_grayscale(image im)
 {
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
-    // TODO Fill this in
+    float weights[] = { 0.299, 0.587, 0.114 };
+
+    for (int c = 0; c < im.c; c++) {
+        for (int h = 0; h < im.h; h++) {
+            for (int w = 0; w < im.w; w++) {
+                gray.data[get_index(gray, w, h, 0)] += weights[c] * im.data[get_index(im, w, h, c)];
+            }
+        }
+    }
     return gray;
 }
 
