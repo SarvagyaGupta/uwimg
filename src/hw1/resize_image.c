e900982e3f29c33f5fb5018ff616eab73b220f3c
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-10-17 19:49:25 -0700

    nn_resize done

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -1,16 +1,51 @@
 #include <math.h>
 #include "image.h"
+#include <assert.h>
+
+#define get_index(im, x, y, c) ((x) + ((im).w * (y)) + ((im).w * (im).h * (c)))
+
+int find_closest_int(float, int);
 
 float nn_interpolate(image im, float x, float y, int c)
 {
-    // TODO Fill in
-    return 0;
+    int x_closest = find_closest_int(x, im.w - 1);
+    int y_closest = find_closest_int(y, im.h - 1);
+    return im.data[get_index(im, x_closest, y_closest, c)];
+}
+
+int find_closest_int(float f, int max) {
+    if (f <= 0) {
+        return 0;
+    } else if (f >= max) {
+        return max;
+    }
+
+    int int_part = (int) f;
+    int val = int_part + (f - int_part < 0.5 ? 0 : 1);
+
+    return val > max ? max : val;
 }
 
 image nn_resize(image im, int w, int h)
 {
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image new_image = make_image(w, h, im.c);
+
+    float x_factor = 1. * im.w / w;
+    float x_shift = x_factor / 2.0 - 0.5;
+
+    float y_factor = 1. * im.h / h;
+    float y_shift = y_factor / 2.0 - 0.5;
+
+    for (int c = 0; c < im.c; c++) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                float x = x_factor * j + x_shift;
+                float y = y_factor * i + y_shift;
+                new_image.data[get_index(new_image, j, i, c)] = nn_interpolate(im, x, y, c);
+            }
+        }
+    }
+    return new_image;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
