70a7466a7c1895a87b870e53fc39aebaa17ab6b0
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-10-17 20:48:14 -0700

    Binary interpolation almost done

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -2,15 +2,14 @@
 #include "image.h"
 #include <assert.h>
 
-#define get_index(im, x, y, c) ((x) + ((im).w * (y)) + ((im).w * (im).h * (c)))
-
 int find_closest_int(float, int);
+float get_contribution(image, int, int, float, float, int);
 
 float nn_interpolate(image im, float x, float y, int c)
 {
     int x_closest = find_closest_int(x, im.w - 1);
     int y_closest = find_closest_int(y, im.h - 1);
-    return im.data[get_index(im, x_closest, y_closest, c)];
+    return get_pixel(im, x_closest, y_closest, c);
 }
 
 int find_closest_int(float f, int max) {
@@ -41,7 +40,8 @@ image nn_resize(image im, int w, int h)
             for (int j = 0; j < w; j++) {
                 float x = x_factor * j + x_shift;
                 float y = y_factor * i + y_shift;
-                new_image.data[get_index(new_image, j, i, c)] = nn_interpolate(im, x, y, c);
+
+                set_pixel(new_image, j, i, c, nn_interpolate(im, x, y, c));
             }
         }
     }
@@ -50,13 +50,41 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    int x_int = (int) x;
+    float x_dec = x - x_int;
+    int y_int = (int) y;
+    float y_dec = y - y_int;
+
+    return get_contribution(im, x_int, y_int, 1 - x_dec, 1 - y_dec, c)
+            + get_contribution(im, x_int + 1, y_int, x_dec, 1 - y_dec, c)
+            + get_contribution(im, x_int, y_int + 1, 1 - x_dec, y_dec, c)
+            + get_contribution(im, x_int + 1, y_int + 1, x_dec, y_dec, c);
+}
+
+float get_contribution(image im, int x, int y, float dx, float dy, int c) {
+    return dx * dy * get_pixel(im, x, y, c);
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+    image new_image = make_image(w, h, im.c);
+
+    float x_factor = 1. * im.w / w;
+    float x_shift = x_factor / 2.0 - 0.5;
+
+    float y_factor = 1. * im.h / h;
+    float y_shift = y_factor / 2.0 - 0.5;
+
+    for (int c = 0; c < im.c; c++) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                float x = x_factor * j + x_shift;
+                float y = y_factor * i + y_shift;
+
+                set_pixel(new_image, j, i, c, bilinear_interpolate(im, x, y, c));
+            }
+        }
+    }
+    return new_image;
 }
 
