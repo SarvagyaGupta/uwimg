24b8a777d3c0a1c5d040d5101dcfb3566f5dbc5f
Sarvagya Gupta <guptasarvagyav@gmail.com>
2019-10-17 21:02:40 -0700

    Redundancy removed

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -2,6 +2,10 @@
 #include "image.h"
 #include <assert.h>
 
+#define func(im, x, y, c, nn) ((nn) ? nn_interpolate((im), (x), (y), (c)) : bilinear_interpolate((im), (x), (y), (c)))
+
+image resize(image, int, int, int);
+
 int find_closest_int(float, int);
 float get_contribution(image, int, int, float, float, int);
 
@@ -27,25 +31,7 @@ int find_closest_int(float f, int max) {
 
 image nn_resize(image im, int w, int h)
 {
-    image new_image = make_image(w, h, im.c);
-
-    float x_factor = 1. * im.w / w;
-    float x_shift = x_factor / 2.0 - 0.5;
-
-    float y_factor = 1. * im.h / h;
-    float y_shift = y_factor / 2.0 - 0.5;
-
-    for (int c = 0; c < im.c; c++) {
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                float x = x_factor * j + x_shift;
-                float y = y_factor * i + y_shift;
-
-                set_pixel(new_image, j, i, c, nn_interpolate(im, x, y, c));
-            }
-        }
-    }
-    return new_image;
+    return resize(im, w, h, 1);
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
@@ -67,6 +53,10 @@ float get_contribution(image im, int x, int y, float dx, float dy, int c) {
 
 image bilinear_resize(image im, int w, int h)
 {
+    return resize(im, w, h, 0);
+}
+
+image resize(image im, int w, int h, int nn) {
     image new_image = make_image(w, h, im.c);
 
     float x_factor = 1. * im.w / w;
@@ -81,7 +71,7 @@ image bilinear_resize(image im, int w, int h)
                 float x = x_factor * j + x_shift;
                 float y = y_factor * i + y_shift;
 
-                set_pixel(new_image, j, i, c, bilinear_interpolate(im, x, y, c));
+                set_pixel(new_image, j, i, c, func(im, x, y, c, nn));
             }
         }
     }
